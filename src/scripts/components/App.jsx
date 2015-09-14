aaf628dc17e0b11db15acf2b68ff1fc92373c40f
Miguel Flores Ruiz de Eguino <miguel.frde@gmail.com>
2015-09-14 00:02:05 -0500

    Change global theme colors

--- src/scripts/components/App.jsx
+++ src/scripts/components/App.jsx
@@ -4,6 +4,7 @@ const mui = require('material-ui');
 const { AppCanvas, Styles } = require('material-ui');
 const ThemeManager = new Styles.ThemeManager();
 const RouteHandler = Router.RouteHandler;
+const Colors = Styles.Colors;
 
 const MenuBar = require('./MenuBar.jsx');
 
@@ -22,6 +23,18 @@ const App = React.createClass({
   },
 
   getChildContext() {
+    ThemeManager.setPalette({
+      primary1Color: Colors.indigo500,
+      primary2Color: Colors.indigo700,
+      primary3Color: Colors.indigo100,
+      accent1Color: Colors.pinkA200,
+      accent2Color: Colors.pinkA400,
+      accent3Color: Colors.pinkA100,
+      textColor: Colors.darkBlack,
+      canvasColor: Colors.fullWhite,
+      borderColor: Colors.faintBlack,
+      disabledColor: 'rgba(0, 0, 0, 0.38)'
+    });
     return {
       muiTheme: ThemeManager.getCurrentTheme()
     };
